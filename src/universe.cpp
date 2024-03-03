8112eb0f34570c588d6c783d3655fb21bc9ffa91
TechnicalFowl <atechnicalfowl@gmail.com>
2024-03-03 13:52:16 -0800

    Universe region handling

--- src/universe.cpp
+++ src/universe.cpp
@@ -8,6 +8,12 @@ void UPlayer::update()
     pos += vel;
 }
 
+void UPlayer::render(TextBuffer& buffer, vec2i origin)
+{
+
+    buffer.setTile(pos - origin, '@', 0xFFFFFFFF, LayerPriority_Actors);
+}
+
 vec2i getOffset(int& i, int& x, int& y)
 {
     y++;
@@ -51,13 +57,44 @@ void Universe::spawn(UActor* a)
     move(a->pos, a);
 }
 
-void Universe::update()
+void Universe::update(vec2i origin)
 {
+    std::vector<vec2i> refresh_regions;
+    for (auto it : regions_generated)
+    {
+        vec2i center(it.key.x << 5 + 16, it.key.y << 5 + 16);
+        if ((center - origin).length() > 116.0f)
+            refresh_regions.push_back(it.key);
+    }
+    for (vec2i r : refresh_regions) regions_generated.erase(r);
+
+    for (int y = origin.y - 70; y < origin.y + 70; y += 32)
+    {
+        int ry = y >> 5;
+        for (int x = origin.x - 70; x < origin.x + 70; x += 32)
+        {
+            int rx = x >> 5;
+
+            if (!regions_generated.find(vec2i(rx, ry)).found)
+            {
+                // Populate region
+
+                regions_generated.insert(vec2i(rx, ry), true);
+            }
+        }
+    }
+
     std::vector<UActor*> moved;
+    std::vector<UActor*> too_far;
     std::vector<vec2i> moved_last_pos;
     for (auto it : actors)
     {
         UActor* a = it.value;
+        if ((a->pos - origin).length() > 100.0f)
+        {
+            too_far.push_back(a);
+            continue;
+        }
         a->update();
 
         if (a->pos != it.key)
@@ -65,6 +102,11 @@ void Universe::update()
             moved.push_back(a);
             moved_last_pos.push_back(it.key);
         }
+    }
+    for (UActor* a : too_far)
+    {
+        delete a;
+        actors.erase(a->pos);
     }
     int i = 0;
     for (UActor* a : moved)
@@ -76,5 +118,9 @@ void Universe::update()
 
 void Universe::render(TextBuffer& buffer, vec2i origin)
 {
-
+    vec2i bl = origin - vec2i(25, 22);
+    for (auto it : actors)
+    {
+        it.value->render(buffer, bl);
+    }
 }
--- src/universe.h
+++ src/universe.h
@@ -5,7 +5,6 @@
 
 #include "vterm.h"
 
-constexpr int UniverseRegionSize = 32;
 struct Ship;
 
 enum class UActorType
@@ -21,6 +20,7 @@ struct UActor
     UActor(UActorType type, vec2i p) : type(type), pos(p) {}
 
     virtual void update() {};
+    virtual void render(TextBuffer& buffer, vec2i origin) = 0;
 };
 
 struct UShip : UActor
@@ -37,16 +37,19 @@ struct UPlayer : UShip
     UPlayer(vec2i p) : UShip(UActorType::Player, p) {}
 
     void update() override;
+
+    void render(TextBuffer& buffer, vec2i origin) override;
 };
 
 struct Universe
 {
     linear_map<vec2i, UActor*> actors;
+    linear_map<vec2i, bool> regions_generated;
 
     void move(vec2i p, UActor* a);
     void spawn(UActor* a);
 
-    void update();
+    void update(vec2i origin);
 
     void render(TextBuffer& buffer, vec2i origin);
 };
